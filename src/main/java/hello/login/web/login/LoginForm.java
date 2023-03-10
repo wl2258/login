ef3742873fd6f53974157bb6df36c1ce7d7dbfe9
wl2258 <77067383+wl2258@users.noreply.github.com>
2023-03-10 12:16:14 +0900

    feat: 로그인 쿠키 처리

---
+++ src/main/java/hello/login/web/login/LoginForm.java
@@ -0,0 +1,13 @@
+package hello.login.web.login;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+
+@Data
+public class LoginForm {
+    @NotEmpty
+    String loginId;
+    @NotEmpty
+    String password;
+}
